c65ea820fc16c305775b26f5505456d188e5a171
Burak Emre Özer <bubabi01@gmail.com>
2019-05-09 18:18:16 +0300

    task1 completed

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from gensim.models import KeyedVectors
 import numpy as np
-from numpy import linalg as LA
 
-path = "GoogleNews-vectors-negative300.bin"
+wv_path = "GoogleNews-vectors-negative300.bin"
+test_path = "word-test.v1.txt"
+relations = ["capital-world", "currency", "city-in-state", "family",
+            "gram1-adjective-to-adverb", "gram2-opposite", "gram3-comparative", "gram6-nationality-adjective"]
+
 
 def cos_sim(v0, v1):
-    return np.dot(v0, v1) / (LA.norm(v0)*LA.norm(v1))
+    return np.dot(v0, v1) / (np.linalg.norm(v0)*np.linalg.norm(v1))
+
 
 def find_analogies(w1, w2, w3):
     params = [w1, w2, w3]
     for word in params:
         if word not in word_vectors:
           print("KeyError:", word, "not in vocabulary")
-          return
+          return ''
 
     a = word_vectors[w1]
     b = word_vectors[w2]
@@ -22,24 +26,77 @@ def find_analogies(w1, w2, w3):
     max_sim = 0
     matched_word = ''
 
-    for idx, word in enumerate(word_vectors.vocab):
-        if word not in params:
-            v1 = word_vectors[word]
-            similarity = cos_sim(v0, v1)
-
-            if similarity > max_sim:
+    for word in word_vectors.vocab:
+        v1 = word_vectors[word]
+        similarity = cos_sim(v0, v1)
+        if similarity > max_sim:
+            if word not in params:
                 max_sim = similarity
                 matched_word = word
 
     print("similarity:", matched_word, max_sim)
-    print(w2, "-", w1, "=", matched_word, "-", w3)
+    # print(w2, "-", w1, "=", matched_word, "-", w3)
 
     return matched_word
 
 
+def test():
+    num_of_correct = 1
+    total_num = 1
+    flag = False
+    with open(test_path) as file:
+        next(file)
 
-word_vectors = KeyedVectors.load_word2vec_format(path, limit=500000, binary=True)
-# result = word_vectors.most_similar(positive=['king', 'he'], negative=['queen'])
-# print("{}: {:.4f}".format(*result[0]))
+        for line in file:
+            if line.split()[0] == ":":
+                continue
 
-find_analogies('Athens', 'Greece', 'Rome')
+            words = line.split()
+            predicted_word = find_analogies(words[0], words[1], words[2])
+
+            if words[3] == predicted_word:
+                num_of_correct += 1
+                total_num += 1
+            else:
+                # print(words[1], "-", words[0], "=", predicted_word, "-", words[2])
+                total_num += 1
+
+        #print("# of Correct Found Tags:", num_of_correct, "\n# of Total Words", total_num)
+        print("Accuracy:", (100*num_of_correct) / total_num)
+
+
+def get_test_set():
+    test_set = set()
+    with open(test_path) as file:
+        next(file)
+
+        for line in file:
+            if line.split()[0] == ":":
+                # print(line)
+                continue
+
+            words = line.split()
+            test_set.add(words[0])
+            test_set.add(words[1])
+            test_set.add(words[2])
+            test_set.add(words[3])
+
+    return test_set
+
+word_vectors = KeyedVectors.load_word2vec_format(wv_path, limit=50000, binary=True)
+word_vectors.syn0norm = word_vectors.syn0
+word_vectors.init_sims(replace=True)
+word_vectors.save("GoogleNews")
+
+word_vectors = KeyedVectors.load('GoogleNews', mmap='r')  # mmap the large matrix as read-only
+word_vectors.syn0norm = KeyedVectors.syn0  # no need to call init_sims
+
+# test_set = get_test_set()
+# wv_dict = dict()
+#
+# for word in word_vectors.vocab:
+#     wv_dict[word] = word_vectors[word]
+
+# print(len(wv_dict))
+# print(len(test_set))
+test()
